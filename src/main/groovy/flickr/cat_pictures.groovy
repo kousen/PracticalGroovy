a24e46eb28a1bc10ec03f8ffb79685dbc239d85c
Kenneth Kousen <ken.kousen@kousenit.com>
2015-05-06 16:51:57 -0500

    Use gpars to load cat images concurrently

--- src/main/groovy/flickr/cat_pictures.groovy
+++ src/main/groovy/flickr/cat_pictures.groovy
@@ -1,5 +1,7 @@
 package flickr
 
+import static groovyx.gpars.GParsPool.*
+
 import groovy.json.*
 import groovy.swing.SwingBuilder
 
@@ -9,13 +11,13 @@ import javax.swing.WindowConstants as WC
 
 String key = new File('flickr_key.txt').text
 String endPoint = 'https://api.flickr.com/services/rest?'
-def params = [method: 'flickr.photos.search',
-              api_key: key,
-              format: 'json',
-              tags: 'cat',
+def params = [method        : 'flickr.photos.search',
+              api_key       : key,
+              format        : 'json',
+              tags          : 'cat',
               nojsoncallback: 1,
-              media: 'photos',
-              per_page: 6]
+              media         : 'photos',
+              per_page      : 6]
 
 // Build URL and download JSON data
 String qs = params.collect { it }.join('&')
@@ -31,16 +33,22 @@ println JsonOutput.prettyPrint(jsonTxt)
 def json = new JsonSlurper().parseText(jsonTxt)
 def photos = json.photos.photo
 
+def images = []
+withPool {
+    images = photos.collectParallel { p ->
+        String url =
+                "http://farm${p.farm}.staticflickr.com/${p.server}/${p.id}_${p.secret}.jpg"
+        url.toURL().bytes
+    }
+}
+
 // build UI using Swing
 new SwingBuilder().edt {
-    frame(title:'Cat pictures', visible: true, pack: true,
-        defaultCloseOperation: WC.EXIT_ON_CLOSE, 
-        layout:new GridLayout(0, 2, 2, 2)) {
-        photos.each { p ->
-            String url =
-                    "http://farm${p.farm}.staticflickr.com/${p.server}/${p.id}_${p.secret}.jpg"
-            String title = p.title
-            label(icon: new ImageIcon(url.toURL()), toolTipText: title)
+    frame(title: 'Cat pictures', visible: true, pack: true,
+            defaultCloseOperation: WC.EXIT_ON_CLOSE,
+            layout: new GridLayout(0, 2, 2, 2)) {
+        images.each {
+            label(icon: new ImageIcon(it))
         }
     }
 }
