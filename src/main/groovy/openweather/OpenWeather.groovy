fdeb6fd135e1704e4ba72983cad32d3b2457339c
Kenneth Kousen <ken.kousen@kousenit.com>
2015-10-21 16:16:21 -0400

    Add new key for OpenWeatherAPI

--- src/main/groovy/openweather/OpenWeather.groovy
+++ src/main/groovy/openweather/OpenWeather.groovy
@@ -3,9 +3,10 @@ package openweather
 import com.google.gson.Gson
 
 class OpenWeather {
-    String base = 'http://api.openweathermap.org/data/2.5/weather?q='
+    String appid = new File('api_key.txt').text
+    String base = 'http://api.openweathermap.org/data/2.5/weather?APPID=$appid&q='
     Gson gson = new Gson()
-    
+
     String getWeather(city='Marlborough', state='CT') {
         String jsonTxt = "$base$city,$state".toURL().text
         gson.fromJson(jsonTxt, Model).toString()
